f8720887c55ee0b2bf2ef29269104e4f9c273a0b
SD Vinh <saddomc224@gmail.com>
2024-10-03 20:16:57 +0700

    Du an hoan thien

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap chieu rong hinh chu nhat:");
+        float width = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap chieu cao hinh chu nhat:");
+        float height = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Chu vi hiunh chu nhat la: {0}",(width+height)*2);
+        Console.WriteLine($"Dien tich hinh chu nhat la {width*height}");
+    }
+}
